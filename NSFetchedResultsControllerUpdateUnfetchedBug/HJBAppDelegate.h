25d113b234509065f79e6d7472c033fc80989772
Heath Borders <heath.borders@jivesoftware.com>
2012-12-23 23:17:51 -0600

    Initial Commit

---
+++ NSFetchedResultsControllerUpdateUnfetchedBug/HJBAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  HJBAppDelegate.h
+//  NSFetchedResultsControllerUpdateUnfetchedBug
+//
+//  Created by Heath Borders on 12/23/12.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HJBAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ NSFetchedResultsControllerUpdateUnfetchedBug/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  NSFetchedResultsControllerUpdateUnfetchedBug
+//
+//  Created by Heath Borders on 12/23/12.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+#import "HJBAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([HJBAppDelegate class]));
+    }
+}
